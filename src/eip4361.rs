de4fa7107c1c8ac9aad1e4bd13ae5e82d182465d
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-11-24 13:37:29 +0100

    fix timestamp re-serialization by storing as validated strings

--- src/eip4361.rs
+++ src/eip4361.rs
@@ -36,15 +36,11 @@ pub struct Message {
     pub version: Version,
     pub chain_id: String,
     pub nonce: String,
-    pub issued_at: TimeStamp,
-    pub expiration_time: Option<TimeStamp>,
-    pub not_before: Option<TimeStamp>,
+    pub issued_at: String,
+    pub expiration_time: Option<String>,
+    pub not_before: Option<String>,
     pub request_id: Option<String>,
     pub resources: Vec<UriString>,
-}
-
-fn rfc3339(t: &TimeStamp) -> String {
-    t.to_rfc3339_opts(SecondsFormat::Secs, true)
 }
 
 impl Display for Message {
@@ -56,12 +52,12 @@ impl Display for Message {
         writeln!(f, "{}{}", VERSION_TAG, self.version as u64)?;
         writeln!(f, "{}{}", CHAIN_TAG, &self.chain_id)?;
         writeln!(f, "{}{}", NONCE_TAG, &self.nonce)?;
-        write!(f, "{}{}", IAT_TAG, &rfc3339(&self.issued_at))?;
+        write!(f, "{}{}", IAT_TAG, &self.issued_at)?;
         if let Some(exp) = &self.expiration_time {
-            write!(f, "\n{}{}", EXP_TAG, &rfc3339(exp))?
+            write!(f, "\n{}{}", EXP_TAG, &exp)?
         };
         if let Some(nbf) = &self.not_before {
-            write!(f, "\n{}{}", NBF_TAG, &rfc3339(nbf))?
+            write!(f, "\n{}{}", NBF_TAG, &nbf)?
         };
         if let Some(rid) = &self.request_id {
             write!(f, "\n{}{}", RID_TAG, rid)?
@@ -110,11 +106,11 @@ fn parse_line<'a, S: FromStr<Err = E>, E: Into<ParseError>>(
     tagged(tag, line).and_then(|s| S::from_str(s).map_err(|e| e.into()))
 }
 
-fn parse_optional<'a, S: FromStr<Err = E>, E: Into<ParseError>>(
+fn tag_optional<'a>(
     tag: &'static str,
     line: Option<&'a str>,
-) -> Result<Option<S>, ParseError> {
-    match parse_line(tag, line).map(|s| Some(s)) {
+) -> Result<Option<&'a str>, ParseError> {
+    match tagged(tag, line).map(|s| Some(s)) {
         Err(ParseError::Format(t)) if t == tag => Ok(None),
         r => r,
     }
@@ -140,28 +136,33 @@ impl FromStr for Message {
         let version = parse_line(VERSION_TAG, lines.next())?;
         let chain_id = parse_line(CHAIN_TAG, lines.next())?;
         let nonce = parse_line(NONCE_TAG, lines.next())?;
-        let issued_at = parse_line(IAT_TAG, lines.next())?;
+        let issued_at = tagged(IAT_TAG, lines.next()).and_then(|iat| {
+            TimeStamp::from_str(iat)?;
+            Ok(iat.into())
+        })?;
 
         let mut line = lines.next();
-        let expiration_time = match parse_optional(EXP_TAG, line)? {
+        let expiration_time = match tag_optional(EXP_TAG, line)? {
             Some(exp) => {
+                TimeStamp::from_str(&exp)?;
                 line = lines.next();
-                Some(exp)
+                Some(exp.into())
             }
             None => None,
         };
-        let not_before = match parse_optional(NBF_TAG, line)? {
+        let not_before = match tag_optional(NBF_TAG, line)? {
             Some(nbf) => {
+                TimeStamp::from_str(nbf)?;
                 line = lines.next();
-                Some(nbf)
+                Some(nbf.into())
             }
             None => None,
         };
 
-        let request_id = match parse_optional(RID_TAG, line)? {
+        let request_id = match tag_optional(RID_TAG, line)? {
             Some(rid) => {
                 line = lines.next();
-                Some(rid)
+                Some(rid.into())
             }
             None => None,
         };
@@ -220,10 +221,17 @@ impl Message {
     }
 
     pub fn valid_now(&self) -> bool {
-        self.not_before.map(|nbf| Utc::now() >= nbf).unwrap_or(true)
+        let now = Utc::now();
+        self.not_before
+            .as_ref()
+            .and_then(|s| TimeStamp::from_str(s).ok())
+            .map(|nbf| now >= nbf)
+            .unwrap_or(true)
             && self
                 .expiration_time
-                .map(|exp| Utc::now() < exp)
+                .as_ref()
+                .and_then(|s| TimeStamp::from_str(s).ok())
+                .map(|exp| now < exp)
                 .unwrap_or(true)
     }
 
@@ -253,10 +261,14 @@ const NBF_TAG: &'static str = "Not Before: ";
 const RID_TAG: &'static str = "Request ID: ";
 const RES_TAG: &'static str = "Resources:";
 
-#[test]
-fn parsing() {
-    // correct order
-    let message = r#"service.org wants you to sign in with your Ethereum account:
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parsing() {
+        // correct order
+        let message = r#"service.org wants you to sign in with your Ethereum account:
 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
 
 I accept the ServiceOrg Terms of Service: https://service.org/tos
@@ -270,13 +282,13 @@ Resources:
 - ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/
 - https://example.com/my-web2-claim.json"#;
 
-    assert!(Message::from_str(message).is_ok());
+        assert!(Message::from_str(message).is_ok());
 
-    assert_eq!(message, &Message::from_str(message).unwrap().to_string());
+        assert_eq!(message, &Message::from_str(message).unwrap().to_string());
 
-    // incorrect order
-    assert!(Message::from_str(
-        r#"service.org wants you to sign in with your Ethereum account:
+        // incorrect order
+        assert!(Message::from_str(
+            r#"service.org wants you to sign in with your Ethereum account:
 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
 
 I accept the ServiceOrg Terms of Service: https://service.org/tos
@@ -289,15 +301,15 @@ Issued At: 2021-09-30T16:25:24Z
 Resources:
 - ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/
 - https://example.com/my-web2-claim.json"#,
-    )
-    .is_err())
-}
+        )
+        .is_err())
+    }
 
-#[test]
-fn validation() {
-    use hex::FromHex;
-    let message = Message::from_str(
-        r#"login.xyz wants you to sign in with your Ethereum account:
+    #[test]
+    fn validation() {
+        use hex::FromHex;
+        let message = Message::from_str(
+            r#"login.xyz wants you to sign in with your Ethereum account:
 0xe2f03cb7a54ddd886da9b0d227bfcb2d61429699
 
 Sign-In With Ethereum Example Statement
@@ -307,10 +319,11 @@ Version: 1
 Chain ID: 1
 Nonce: k13wuejc
 Issued At: 2021-11-12T17:37:48.462Z"#,
-    )
-    .unwrap();
-    let correct = <[u8; 65]>::from_hex(r#"795110331a07a4d475419fbdb346feb4c0579dcc8228989964474e07d98dbf425f38776cd6ca037f58288acc7b15e720c9cecac988479177fb70592f2391aaff1b"#).unwrap();
-    assert!(message.verify_eip191(correct).is_ok());
-    let incorrect = <[u8; 65]>::from_hex(r#"895110331a07a4d475419fbdb346feb4c0579dcc8228989964474e07d98dbf425f38776cd6ca037f58288acc7b15e720c9cecac988479177fb70592f2391aaff1b"#).unwrap();
-    assert!(message.verify_eip191(incorrect).is_err());
+        )
+        .unwrap();
+        let correct = <[u8; 65]>::from_hex(r#"795110331a07a4d475419fbdb346feb4c0579dcc8228989964474e07d98dbf425f38776cd6ca037f58288acc7b15e720c9cecac988479177fb70592f2391aaff1b"#).unwrap();
+        assert!(message.verify_eip191(correct).is_ok());
+        let incorrect = <[u8; 65]>::from_hex(r#"895110331a07a4d475419fbdb346feb4c0579dcc8228989964474e07d98dbf425f38776cd6ca037f58288acc7b15e720c9cecac988479177fb70592f2391aaff1b"#).unwrap();
+        assert!(message.verify_eip191(incorrect).is_err());
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,1 @@
 pub mod eip4361;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
