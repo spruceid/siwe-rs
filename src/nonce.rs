4f2bd8916783b81fb491989203c47ce2a0976642
K Rhoda <kelsey.rhoda@spruceid.com>
2022-01-12 17:40:18 -0700

    Updates to increase nonce entropy

--- src/nonce.rs
+++ src/nonce.rs
@@ -4,7 +4,7 @@ use rand::{thread_rng, Rng};
 pub fn generate_nonce() -> String {
     thread_rng()
         .sample_iter(&Alphanumeric)
-        .take(8)
+        .take(11)
         .map(char::from)
         .collect()
 }
